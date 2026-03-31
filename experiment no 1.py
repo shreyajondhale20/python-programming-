e41e3be0049866001f38642dd418e2e9814fa2c2
shreyajondhale20 <shreyajondhale94@gmail.com>
2026-03-31 23:16:31 +0530

    Update experiment no 1.py

--- experiment no 1.py
+++ experiment no 1.py
@@ -1,5 +1,5 @@
 #integer
-Roll_no=207
+Roll_no=220
 print(type(Roll_no))
 
 #float
@@ -7,7 +7,7 @@ b=10.5
 print(type(b))
 
 #string
-c="nagesh"
+c="shreya"
 print(type(c))
 
 #boolean
@@ -23,7 +23,7 @@ f=(10,20,30)
 print(type(f))
 
 #dictionary
-g={"name":"nagesh",
+g={"name":"shreya",
    "age":20}
 print(type(g))
 
